5381c1723f6e28e24c778b7f365c284790de41c7
ömer faruk sezer <omerseze25@gmail.com>
2023-11-14 13:37:44 +0300

    Initial Commit

---
+++ MachineLearningImageClassificationRecognation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningImageClassificationRecognation
+//
+//  Created by ömer  faruk sezer on 14.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
